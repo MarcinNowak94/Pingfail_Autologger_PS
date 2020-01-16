8c0a01fe7d2e597bde2e07bc710fd0514e84674b
Marcin Nowak <marcin94nowak@gmail.com>
2020-01-16 18:53:23 +0100

    Add files via upload
    Initializing repo, minimal version

---
+++ PingFail_Autolloger_PS.ps1
@@ -0,0 +1,11 @@
+﻿$IP= "8.8.8.8"
+
+do {
+    if (!(Test-Connection $IP -count 1)) {
+        write-host "$(get-date -f yyyy-MM-dd_hh-mm-ss)`tping failed"
+    } else {
+        write-host "$(get-date -f yyyy-MM-dd_hh-mm-ss)`tping succedded"
+    }
+    sleep 5
+    $result
+} while (!([ System.Environment ]:: HasShutdownStarted ))
